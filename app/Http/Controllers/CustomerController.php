7b439e7a1ab4df2904f800f11112d6cfce5cf4ac
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2024-06-14 15:58:27 -0400

    API: Customer CRUD

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+use App\Models\User;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(User $user)
+    {
+        $customers = Customer::where('user_id', $user->id)
+            ->get();
+        return [
+            'customers' => $customers
+        ];
+    }
+
+    /**
+     * Create a new Customer.
+     */
+    public function store(Request $request, User $user)
+    {
+        $request->merge([
+            'user_id' => $user->id
+        ]);
+        $customer = Customer::create(
+            $request->only([
+                'full_name',
+                'document_id',
+                'telephone',
+                'email',
+                'address',
+                'notes',
+                'user_id',
+            ])
+        );
+        return response()
+            ->json([
+                'customer' => $customer
+            ], 201);
+    }
+
+    /**
+     * Retrieve the specified Customer.
+     */
+    public function show(User $user, Customer $customer)
+    {
+        if( $customer->user_id !== $user->id ) {
+            return response()
+                ->json([
+                    'code' => 'Forbidden',
+                    'message' => 'Unauthorized access to this resource.'
+                ], 403);
+        }
+        return response()
+            ->json([
+                'customer' => $customer
+            ]);
+    }
+
+    /**
+     * Update the specified Customer.
+     */
+    public function update(Request $request, User $user, Customer $customer)
+    {
+        if( $customer->user_id !== $user->id ) {
+            return response()
+                ->json([
+                    'code' => 'Forbidden',
+                    'message' => 'Unauthorized access to this resource.'
+                ], 403);
+        }
+        $customer->fill(
+            $request->only([
+                'full_name',
+                'document_id',
+                'telephone',
+                'email',
+                'address',
+                'notes',
+                'user_id',
+            ])
+        );
+        $customer->save();
+        return response()
+            ->json([
+                'customer' => $customer
+            ]);
+    }
+
+    /**
+     * Delete the specified Customer.
+     */
+    public function destroy(User $user, Customer $customer)
+    {
+        if( $customer->user_id !== $user->id ) {
+            return response()
+                ->json([
+                    'code' => 'Forbidden',
+                    'message' => 'Unauthorized access to this resource.'
+                ], 403);
+        }
+        $result = $customer->delete();
+        return response()
+            ->json([
+                'result' => $result
+            ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,31 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::any('/info', function(Request $request) {
+    return response()->json([
+        'app' => 'Payame API',
+        'version' => 1,
+    ]);
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::resource('users', UserController::class);
+Route::resource('{user}/customers', CustomerController::class)
+    ->missing(function (Request $request) {
+        return response()->json([
+            'code' => 'Not Found',
+            'message' => 'Customer not found.'
+        ], 404);
+    });
+
+
+// Route::apiResources([
+//     'photos' => PhotoController::class,
+//     'posts' => PostController::class,
+// ]);
