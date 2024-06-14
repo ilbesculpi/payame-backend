9d07df87ba42d5db98447a88a34509b5fdd8d32a
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2024-06-14 10:31:59 -0400

    database migrations: clients / loans / payments

---
+++ database/migrations/2024_06_14_035331_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('full_name');
+            $table->string('document_id')
+                ->nullable();
+            $table->string('telephone')
+                ->nullable();
+            $table->string('email')
+                ->nullable();
+            $table->string('address')
+                ->nullable();
+            $table->mediumText('notes')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
---
+++ database/migrations/2024_06_14_035342_create_loans_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained();
+            $table->foreignId('customer_id')
+                ->constrained();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('method', ['simple', 'fixed', 'compound', 'annual'])
+                ->default('simple');
+            $table->float('interest');
+            $table->decimal('initial_capital', total: 8, places: 2);
+            $table->decimal('capital', total: 8, places: 2);
+            $table->decimal('quota', total: 8, places: 2);
+            $table->enum('frequency', ['monthly', 'single', 'open'])
+                ->default('monthly');
+            $table->smallInteger('day_of_month');
+            $table->smallInteger('payments');
+            $table->smallInteger('payments_pending')
+                ->default(0);
+            $table->smallInteger('payments_received')
+                ->default(0);
+            $table->enum('status', ['active', 'paused', 'completed'])
+                ->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
