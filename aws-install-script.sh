2bfd45476e6b40925b22cb996fdbc4e782ffbe29
Ilbert Esculpi <ilbert.esculpi@gmail.com>
2024-09-26 17:04:56 -0400

    aws install script

--- aws-install-script.sh
+++ aws-install-script.sh
@@ -7,5 +7,17 @@ sudo apt install -y php php-cli php-common php-mbstring php-xml php-zip php-mysq
 sudo apt install -y curl php-cli php-mbstring git unzip
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
+# Install MySQL
+sudo apt install -y mysql-server
+sudo systemctl start mysql.service
+
 # Install nginx
 sudo apt install -y nginx
+
+# Manage /var/www permissions
+sudo usermod -a -G www-data $USER
+sudo chown -R "$USER":www-data /var/www/
+sudo chmod -R 0755 /var/www/
+
+# Clone git project
+git clone git@github.com:ilbesculpi/payame-backend.git /var/www/payame
